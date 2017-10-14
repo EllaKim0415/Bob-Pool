eb04d54b8a46cdeaab153f8bd6e8db057daaa067
dmsqls0427k <danaeblee@gmail.com>
2017-10-14 03:30:26 -0400

    makenewRoom

--- BobPool/app/src/main/java/com/example/khrst/bobpool/MakeNewRoomActivity.java
+++ BobPool/app/src/main/java/com/example/khrst/bobpool/MakeNewRoomActivity.java
@@ -1,13 +1,51 @@
 package com.example.khrst.bobpool;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.DatePicker;
 public class MakeNewRoomActivity extends AppCompatActivity {
-
+    private Button addButton;
+    private EditText newName, newDate, newTime, newCapacity, newNotes;
+    private Calendar newCalendar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_new_room);
+        newDate = (EditText) findViewById(R.id.newDate);
+        newCalendar = Calendar.getInstance();
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                newCalendar.set(Calendar.YEAR, year);
+                newCalendar.set(Calendar.MONTH, month);
+                newCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel(newDate);
+
+            }
+        };
+        newDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(MakeNewRoomActivity.this, date, newCalendar
+                        .get(Calendar.YEAR), newCalendar.get(Calendar.MONTH),
+                        newCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+    private void updateLabel(EditText t) {
+
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        t.setText(sdf.format(newCalendar.getTime()));
     }
 }
