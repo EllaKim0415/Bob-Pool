958baf8c91b6a8e49f35db79517e58b65a04b01c
dmsqls0427k <danaeblee@gmail.com>
2017-10-14 14:41:45 -0400

    makenewRoom

--- BobPool/app/src/main/java/com/example/khrst/bobpool/LoginActivity.java
+++ BobPool/app/src/main/java/com/example/khrst/bobpool/LoginActivity.java
@@ -93,7 +93,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         registerButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this, RestaurantActivity.class));
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
             }
         });
         mLoginFormView = findViewById(R.id.login_form);
