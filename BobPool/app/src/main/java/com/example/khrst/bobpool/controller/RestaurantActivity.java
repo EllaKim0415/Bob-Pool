db82566e64cf7479fae9ed74a4fce5fdec5c2d77
unknown <hobmobo@gmail.com>
2017-10-14 15:52:05 -0400

    Restaurant Activity and Pool Class editted

--- BobPool/app/src/main/java/com/example/khrst/bobpool/controller/RestaurantActivity.java
+++ BobPool/app/src/main/java/com/example/khrst/bobpool/controller/RestaurantActivity.java
@@ -9,56 +9,42 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 import android.widget.ListView;
 
 public class RestaurantActivity extends ListActivity {
-    private ListView lv;
-    List<String> pools;
+    ListView lv;
+    static List<String> pools = new ArrayList<>();
+    FirebaseDatabase database;
+    DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference();
         super.onCreate(savedInstanceState);
         lv = (ListView) findViewById(R.id.lv);
-        pools = new ArrayList<String>();
-        DatabaseReference poolsDatabaseRef;
-        poolsDatabaseRef = FirebaseDatabase.getInstance().getReference("pools");
-        //poolsDatabase.child("pool1").getKey();
-        poolsDatabaseRef.addChildEventListener(new ChildEventListener() {
+        databaseReference.child("pool").addValueEventListener(new ValueEventListener() {
             @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                pools.add(dataSnapshot.getValue().toString());
-                //System.out.println(dataSnapshot.getValue().toString());
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-                System.out.println("child changed");
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-                System.out.println("child removed");
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-                System.out.println("child moved");
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot> poolData = dataSnapshot.getChildren();
+                for (DataSnapshot pool : poolData) {
+                    pools.add(pool.child("title").getValue().toString());
+                }
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-                System.out.println("cancelled");
+
             }
         });
         if (pools.isEmpty()) {
-            System.out.println("000015");
+            System.out.println("EMPTY");
         }
         for (String i: pools) {
             System.out.println(i);
-            System.out.println("0");
         }
 //        ArrayAdapter<String> adapter = new ArrayAdapter<String>(RestaurantActivity.this,
 //                android.R.layout.simple_list_item_1, pools);
