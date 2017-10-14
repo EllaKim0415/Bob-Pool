52a223b3294d8019fa73af1d4a10ea732f931ee4
EunBin Lee <danaeblee@gmail.com>
2017-10-14 00:10:13 -0400

    Add files via upload

---
+++ BobPool/app/src/main/java/com/example/khrst/bobpool/MakeNewRoomActivity.java
@@ -0,0 +1,13 @@
+package com.example.khrst.bobpool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MakeNewRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_new_room);
+    }
+}
