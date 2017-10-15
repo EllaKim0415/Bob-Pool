7692889d94ccf73acc30b7a02afa14d9c9bb3af2
EllaKim0415 <khrstar7979@gmail.com>
2017-10-15 03:00:40 -0400

    little change

--- BobPool/app/src/main/java/com/example/khrst/bobpool/controller/PoolDetailActivity.java
+++ BobPool/app/src/main/java/com/example/khrst/bobpool/controller/PoolDetailActivity.java
@@ -17,6 +17,7 @@ import com.google.firebase.database.ValueEventListener;
 public class PoolDetailActivity extends AppCompatActivity {
     private TextView restaurant, name, date, time, currentNum, maxNum, notes;
     private static Pool selected;
+    private String selectedKey;
     private Button joinButton;
     private String key;
     FirebaseDatabase database;
@@ -29,24 +30,24 @@ public class PoolDetailActivity extends AppCompatActivity {
         database = FirebaseDatabase.getInstance();
         databaseReference = database.getReference(MapsActivity.getSelectedRestaurant());
         //selected = RestaurantActivity.getSelectedPool();
+        selectedKey = RestaurantActivity.getSelectedKey();
 
-        key = RestaurantActivity.getSelectedKey();
-        databaseReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Iterable<DataSnapshot> children = dataSnapshot.getChildren();
-                for (DataSnapshot child : children) {
-                    if (child.getKey().equals(key)) {
-                        selected = child.getValue(Pool.class);
-                    }
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
+//        databaseReference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+//                for (DataSnapshot child : children) {
+//                    if (child.getKey().equals(key)) {
+//                        selected = child.getValue(Pool.class);
+//                    }
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
         restaurant = (TextView) findViewById(R.id.restaurantText);
         restaurant.setText(selected.getRestaurant());
         name = (TextView) findViewById(R.id.nameText);
@@ -63,9 +64,6 @@ public class PoolDetailActivity extends AppCompatActivity {
         if (!selected.getNotes().equals("")) {
             notes.setText(selected.getNotes());
         }
-
-        database = FirebaseDatabase.getInstance();
-        databaseReference = database.getReference();
 
         joinButton = (Button) findViewById(R.id.joinButton);
         joinButton.setOnClickListener(new View.OnClickListener(){
