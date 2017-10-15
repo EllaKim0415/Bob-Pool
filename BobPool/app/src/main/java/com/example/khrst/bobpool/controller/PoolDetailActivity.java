47f99855d536e66fbad731591ac0c7a9c4f23533
EllaKim0415 <khrstar7979@gmail.com>
2017-10-14 22:34:01 -0400

    PoolDetailActivity

---
+++ BobPool/app/src/main/java/com/example/khrst/bobpool/controller/PoolDetailActivity.java
@@ -0,0 +1,17 @@
+package com.example.khrst.bobpool.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.khrst.bobpool.R;
+
+public class PoolDetailActivity extends AppCompatActivity {
+
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pool_detail);
+    }
+}
